e250189af6451b41f6c663a7e0f850ee2878fdb2
Ali Mirza <ali8751805@gmail.com>
2024-12-03 17:06:42 +0500

    Time: 11 ms (36.28%) | Memory: 17.2 MB (7.10%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -3,12 +3,13 @@ class Solution:
         x = str(x)
         left = 0
         right = len(x) - 1
-        is_palindrome = True
+        # is_palindrome = True
         while left < right:
             if x[left] != x[right]:
-                is_palindrome = False
-                break
+                # is_palindrome = False
+                return False
+                # break
             else:
                 left += 1
                 right -= 1
-        return is_palindrome
+        return True
