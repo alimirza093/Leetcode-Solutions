6b22a3427e0a2aa7e3aa1521d3073dc5b310a0be
Ali Mirza <ali8751805@gmail.com>
2024-12-06 00:31:52 +0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.27%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -7,7 +7,6 @@
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-
         def preOrder(root):
             if not root:
                 return
@@ -15,6 +14,5 @@ class Solution:
                 res.append(root.val)
                 preOrder(root.left)
                 preOrder(root.right)
-
         preOrder(root)
         return res
